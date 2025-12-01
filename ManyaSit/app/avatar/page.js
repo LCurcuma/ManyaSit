d10cf74c851a41ecc89c455d8e95231d7857d4e5
LCurcuma <zhihnadya2002@gmail.com>
2025-12-01 18:16:19 +0100

    added styling of avatar page (evening ver)

--- ManyaSit/app/avatar/page.js
+++ ManyaSit/app/avatar/page.js
@@ -1,10 +1,19 @@
 "use client";
 
-import { useState } from "react";
+import { useState, useEffect } from "react";
+import styles from "./page.module.scss";
 
 export default function AvatarUploadPage() {
+
   const [file, setFile] = useState(null);
   const [message, setMessage] = useState("");
+  const [time, setTime] = useState();
+
+  useEffect(() => {
+  let d = new Date();
+    let t = d.getHours();
+    setTime(t);
+  }, [])
 
   async function uploadAvatar(e) {
     e.preventDefault();
@@ -12,7 +21,7 @@ export default function AvatarUploadPage() {
 
     const token = localStorage.getItem("token");
     if (!token) {
-      setMessage("Ви не авторизовані.");
+      setMessage("Вы не авторизованы");
       return;
     }
 
@@ -29,28 +38,110 @@ export default function AvatarUploadPage() {
 
     const data = await res.json();
     if (res.ok) {
-      setMessage("Аватар успішно змінено!");
+      setMessage("Аватар успешно изменен!");
     } else {
-      setMessage(data.error || "Помилка завантаження.");
+      setMessage(data.error || "Ошибка загрузки");
     }
   }
 
   return (
-    <div style={{ padding: 20 }}>
-      <h1>Завантаження аватару</h1>
+    <>
+      {time >= 0 && time < 6 && (
+        <div className={styles.avatar_main_container_night}>
+          <h1 className={styles.h1}>Смена аватара</h1>
 
-      <form onSubmit={uploadAvatar}>
-        <input
-          type="file"
-          accept="image/*"
-          onChange={(e) => setFile(e.target.files?.[0] || null)}
-        />
-        <br />
-        <br />
-        <button type="submit">Завантажити</button>
-      </form>
+          <form onSubmit={uploadAvatar} className={styles.form}>
+            <input
+              type="file"
+              accept="image/*"
+              onChange={(e) => setFile(e.target.files?.[0] || null)}
+            />
+            <button type="submit">Завантажити</button>
+          </form>
 
-      {message && <p>{message}</p>}
-    </div>
+          {message && <p>{message}</p>}
+          <a href="/main">Назад</a>
+        </div>
+      )}
+      {time >= 6 && time < 12 && (
+        <div className={styles.avatar_main_container_morning}>
+          <h1 className={styles.h1}>Смена аватара</h1>
+
+          <form onSubmit={uploadAvatar} className={styles.form}>
+            <input
+              type="file"
+              accept="image/*"
+              onChange={(e) => setFile(e.target.files?.[0] || null)}
+            />
+            <button type="submit">Завантажити</button>
+          </form>
+
+          {message && <p>{message}</p>}
+          <a href="/main">Назад</a>
+        </div>
+      )}
+      {time >= 12 && time < 16 && (
+        <div className={styles.avatar_main_container_day}>
+          <h1 className={styles.h1}>Смена аватара</h1>
+
+          <form onSubmit={uploadAvatar} className={styles.form}>
+            <input
+              type="file"
+              accept="image/*"
+              onChange={(e) => setFile(e.target.files?.[0] || null)}
+            />
+            <button type="submit">Завантажити</button>
+          </form>
+
+          {message && <p>{message}</p>}
+          <a href="/main">Назад</a>
+        </div>
+      )}
+      {time >= 16 && time < 22 && (
+        <div className={styles.avatar_main_container_evening}>
+          <h1 className={styles.h1}>Смена аватара</h1>
+
+          <form onSubmit={uploadAvatar} className={styles.form}>
+            <label className={styles.upload_avatar_evening}>
+              Выбрать аватар
+              <input
+                type="file"
+                accept="image/*"
+                onChange={(e) => setFile(e.target.files?.[0] || null)}
+                className={styles.hide}
+              />
+            </label>
+            {file && (
+              <img
+                src={URL.createObjectURL(file)}
+                alt="Preview"
+                className={styles.preview_image}
+              />
+            )}
+            <button type="submit" className={styles.submit_evening}>Завантажити</button>
+          </form>
+
+          {message && <p>{message}</p>}
+          <a href="/main" className={styles.link}>Назад</a>
+        </div>
+      )}
+      {time >= 22 && time < 24 && (
+        <div className={styles.avatar_main_container_night}>
+          <h1 className={styles.h1}>Смена аватара</h1>
+
+          <form onSubmit={uploadAvatar} className={styles.form}>
+            <input
+              type="file"
+              accept="image/*"
+              onChange={(e) => setFile(e.target.files?.[0] || null)}
+            />
+            <button type="submit">Завантажити</button>
+          </form>
+
+          {message && <p>{message}</p>}
+          <a href="/main">Назад</a>
+        </div>
+      )}
+    </>
   );
 }
